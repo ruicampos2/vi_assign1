f1b3a7d2f9d8d22b329bf707798a8d410e15ba1c
Rui Campos <ruigabriel2@ua.pt>
2024-11-13 00:49:14 +0000

    scale

--- js/index_script.js
+++ js/index_script.js
@@ -214,45 +214,48 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   function renderBarChart(data, containerId, yLabel) {
-      d3.select(containerId).selectAll("*").remove();
+    d3.select(containerId).selectAll("*").remove();
 
-      const margin = { top: 20, right: 30, bottom: 40, left: 90 };
-      const width = 400 - margin.left - margin.right;
-      const height = 300 - margin.top - margin.bottom;
+    const margin = { top: 20, right: 30, bottom: 40, left: 90 };
+    const width = 500 - margin.left - margin.right;
+    const height = 300 - margin.top - margin.bottom;
 
-      const svg = d3.select(containerId)
-          .append("svg")
-          .attr("width", width + margin.left + margin.right)
-          .attr("height", height + margin.top + margin.bottom)
-          .append("g")
-          .attr("transform", `translate(${margin.left},${margin.top})`);
+    const svg = d3.select(containerId)
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`);
 
-      const x = d3.scaleLinear()
-          .domain([0, d3.max(data, d => d.value)])
-          .range([0, width]);
+    // Define a escala do eixo X com um limite fixo de 50
+    const x = d3.scaleLinear()
+        .domain([0, 50]) // Altere para o valor máximo desejado
+        .range([0, width]);
 
-      const y = d3.scaleBand()
-          .domain(data.map(d => d.country))
-          .range([0, height])
-          .padding(0.1);
+    const y = d3.scaleBand()
+        .domain(data.map(d => d.country))
+        .range([0, height])
+        .padding(0.1);
 
-      svg.selectAll("rect")
-          .data(data)
-          .enter()
-          .append("rect")
-          .attr("x", x(0))
-          .attr("y", d => y(d.country))
-          .attr("width", d => x(d.value))
-          .attr("height", y.bandwidth())
-          .attr("fill", "#69b3a2");
+    svg.selectAll("rect")
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("x", x(0))
+        .attr("y", d => y(d.country))
+        .attr("width", d => x(d.value))
+        .attr("height", y.bandwidth())
+        .attr("fill", "#69b3a2");
 
-      svg.append("g").call(d3.axisLeft(y));
-      svg.append("g").attr("transform", `translate(0,${height})`).call(d3.axisBottom(x));
+    svg.append("g").call(d3.axisLeft(y));
+    svg.append("g").attr("transform", `translate(0,${height})`).call(d3.axisBottom(x));
 
-      svg.append("text")
-          .attr("x", width / 2)
-          .attr("y", height + margin.bottom - 5)
-          .attr("text-anchor", "middle")
-          .text(yLabel);
+    svg.append("text")
+        .attr("x", width / 2)
+        .attr("y", height + margin.bottom - 5)
+        .attr("text-anchor", "middle")
+        .text(yLabel);
   }
+
+
 });
